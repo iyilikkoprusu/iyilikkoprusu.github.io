76f374e17cde28e5e565973750980b18896b3c4f
Eyüp Ensar <eensarerkul@gmail.com>
2026-05-07 15:54:50 +0300

    Update appwrite.js

--- appwrite.js
+++ appwrite.js
@@ -25,30 +25,3 @@ async function oyuncakAyirt(urunId, urunAdi, fiyat) {
         alert("Bir hata oluştu, ID'leri kontrol et!");
     }
 }
-async function oyuncakAyirt(urunId, urunAdi, fiyat) {
-    const isim = prompt("Adın Soyadın:");
-    const sinif = prompt("Sınıfın (Örn: 8-C):");
-
-    if(!isim || !sinif) return alert("HATA! Bilgileri girmeden sipariş verilemez.");
-
-    try {
-        // 1. ADIM: Siparişi oluştur
-        await databases.createDocument('DATABASE_ID', 'SIPARISLER_ID', 'unique()', {
-            "urun_adi": urunAdi,
-            "alici_ad_soyad": isim,
-            "sinif": sinif,
-            "fiyat": fiyat,
-            "durum": "Ödeme Bekliyor" // Varsayılan durum
-        });
-
-        // 2. ADIM: Ürünü 'Satıldı' olarak işaretle (Kimse bir daha almasın)
-        await databases.updateDocument('DATABASE_ID', 'URUNLER_ID', urunId, {
-            "durum": "Satıldı" 
-        });
-
-        alert(`Tamamdır ${isim}! Oyuncak senin için ayırdık. Teneffüste yetkili ekibe ürün ücretini getir, sipariş verdiğin oyuncağı al. Allah kabul etsin!`);
-        location.reload(); // Sayfayı yenile ki ürün 'Satıldı' görünsün
-    } catch (error) {
-        alert("Bir hata oluştu, ID'leri kontrol et!");
-    }
-}
